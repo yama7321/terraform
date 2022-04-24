f8effe433e7501eaa919e00f1c5d772ec9cba739
ryamaoka-cq <74167612+ryamaoka-cq@users.noreply.github.com>
2022-04-24 19:28:56 +0900

    terraform basic syntaxes

---
+++ example/http_server/main.tf
@@ -0,0 +1,33 @@
+variable "instance_type" {}
+
+resource "aws_instance" "default" {
+  ami = "ami-0c3fd0f5d33134a76"
+  vpc_security_group_ids = [aws_security_group.default.id]
+  instance_type = var.instance_type
+  user_data = <<EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd.service
+  EOF
+}
+
+resource "aws_security_group" "default" {
+  name = "ec2"
+
+  ingress {
+    from_port = 80
+    protocol  = "tcp"
+    to_port   = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "example_public_dns" {
+  value = aws_instance.default.public_dns
+}
